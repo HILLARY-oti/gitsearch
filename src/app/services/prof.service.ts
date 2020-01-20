5ed18f667c013eab26fee6d26a372ab61788a133
HILLARY-oti <hillaryotieno646@gmail.com>
2020-01-20 09:46:28 +0300

    add privet clientid and clientsecret

--- src/app/services/prof.service.ts
+++ src/app/services/prof.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfService {
 
-  constructor() { }
+  private username:string;
+  private clinetid = '6720367aabe103c1061e';
+  private clinetsecret = '7aa30f913c49546e09a26f2a7c3286865cae7f50';
+  constructor(private http:HttpHeaders) { }
 }
