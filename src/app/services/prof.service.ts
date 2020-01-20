b021cc448f4aaff150caede1acd20034d1267ab5
HILLARY-oti <hillaryotieno646@gmail.com>
2020-01-20 10:36:17 +0300

    add return message

--- src/app/services/prof.service.ts
+++ src/app/services/prof.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,11 @@ export class ProfService {
   private clinetid = '6720367aabe103c1061e';
   private clinetsecret = '7aa30f913c49546e09a26f2a7c3286865cae7f50';
 
-  constructor(private http:HttpHeaders) {
+  constructor(private _http:HttpHeaders) {
     console.log("Service is ready");
     this.username = 'HILLARY-oti';
    }
    getprofInfo(){
-    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clinetid + "&client_secret=" + this.clinetsecret);
+    return this._http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clinetid + "&client_secret=" + this.clinetsecret);
    }
 }
