0c6454b930567b8438ca92ea4baa1270a484c90a
HILLARY-oti <hillaryotieno646@gmail.com>
2020-01-20 09:47:19 +0300

    add console.log message7

--- src/app/services/prof.service.ts
+++ src/app/services/prof.service.ts
@@ -9,5 +9,9 @@ export class ProfService {
   private username:string;
   private clinetid = '6720367aabe103c1061e';
   private clinetsecret = '7aa30f913c49546e09a26f2a7c3286865cae7f50';
-  constructor(private http:HttpHeaders) { }
+
+  constructor(private http:HttpHeaders) {
+    console.log("Service is ready");
+   }
+
 }
